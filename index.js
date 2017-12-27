30631371f9fc7bb48bdd86a8718e8ceb6e027e57
Abhijit <abhijit.khanna@live.com>
2017-12-27 18:26:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,10 @@ function keepGoing(array, changeValue, skipValue) {
 
 function findBy(array, findFn) {
   for (let i = 0; i < array.length; i++) {
-    if (findFn ===)
+    if (array[i] === findFn) {
+      return (array[i])
+      break
+    }
   }
-
+  return(null)
 }
